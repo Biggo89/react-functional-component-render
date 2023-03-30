3d6418b11cd33c723daa041f28953e96b5227637
Biggo89 <aledev.89@gmail.com>
2023-03-30 11:30:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,40 +5,52 @@ import * as data from "./messages.json";
 // Functional component
 const useConstructor = (callBack = () => {}) => {
   const [hasBeenCalled, setHasBeenCalled] = useState(false);
+  console.log("constructor body " + hasBeenCalled);
   if (hasBeenCalled) return;
   callBack();
   setHasBeenCalled(true);
 };
+const Child = () => {
+  console.log("child body");
+  useEffect(() => {
+    console.log("Child useEffect");
+  }, []);
+  useLayoutEffect(() => {
+    console.log("Child useLayoutEffect");
+  }, []);
+  return <h3>Figlio</h3>;
+};
 
 const App = () => {
   useConstructor(() => {
-    console.log("Occurs ONCE, BEFORE the initial render.");
+    console.log("padre constructor");
   });
-  const [counter, setCounter] = useState(0);
-  const [constructorHasRun, setConstructorHasRun] = useState(false);
+  //const [counter, setCounter] = useState(0);
+  // const [constructorHasRun, setConstructorHasRun] = useState(false);
 
   useEffect(() => {
-    console.log("Occurs ONCE, but it occurs AFTER the initial render.");
+    console.log(" PADRE useEffect");
   }, []);
 
   useLayoutEffect(() => {
-    console.log("Occurs ONCE, but it still occurs AFTER the initial render.");
+    console.log("PADRE uselayouteffect");
   }, []);
 
-  const constructor = () => {
-    if (constructorHasRun) return;
-    console.log("Inline constructor()");
-    setConstructorHasRun(true);
-  };
+  // const constructor = () => {
+  //   if (constructorHasRun) return;
+  //   console.log("Inline constructor()");
+  //   setConstructorHasRun(true);
+  // };
 
-  constructor();
-  console.log("Occurs EVERY time the component is invoked.");
+  // constructor();
+  console.log("PADRE body");
   return (
     <>
-      <div>Counter: {counter}</div>
+      {/* <div>Counter: {counter}</div>
       <div style={{ marginTop: 20 }}>
         <button onClick={() => setCounter(counter + 1)}>Increment</button>
-      </div>
+      </div> */}
+      <Child></Child>
     </>
   );
 };
